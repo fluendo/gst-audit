64a6c4e133a80b8022e4a6d448b1fcadf49b2a05
Jorge Zapata <jorgeluis.zapata@gmail.com>
2025-12-01 17:56:39 +0100

    Remove any reference to the NodeTreeManager

--- gstaudit/lib/index.ts
+++ gstaudit/lib/index.ts
@@ -1,7 +1,5 @@
 export { getLayoutedElements } from './layout';
 export { getConfig } from './config';
-export { NodeTreeManager } from './NodeTreeManager';
-export type { NodeTree } from './NodeTreeManager';
 export { ElementTreeManager } from './ElementTreeManager';
 export type { ElementTree, ElementPad } from './ElementTreeManager';
 export { getTheme, setTheme, gstDotTheme } from './theme';
